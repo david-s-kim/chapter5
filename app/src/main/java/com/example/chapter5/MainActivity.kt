1765000324446345ee5aedebf73ac24762dd6fea
david-s-kim <mycodingtool9@gmail.com>
2023-04-10 21:11:29 +0900

    계산기 앱

---
+++ app/src/main/java/com/example/chapter5/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.chapter5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.example.chapter5.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val firstNumberText = StringBuilder("")
+    private val secondNumberText = StringBuilder("")
+    private val operatorText = StringBuilder("")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    fun numberClicked(view: View) {
+        val numberString = (view as? Button)?.text.toString() ?: ""
+        val numberText = if (operatorText.isEmpty()) firstNumberText else secondNumberText
+
+        numberText.append(numberString)
+        updateEquationTextView()
+    }
+
+    fun clearClicked(view: View) {
+        firstNumberText.clear()
+        secondNumberText.clear()
+        operatorText.clear()
+        updateEquationTextView()
+        binding.resultTextView.text = "0"
+    }
+
+    fun equalClicked(view: View) {
+        if(firstNumberText.isEmpty() || secondNumberText.isEmpty() || operatorText.isEmpty()) {
+            Toast.makeText(this, "올바르지 않은 수식 입니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val firstNumber = firstNumberText.toString().toInt()
+        val secondNumber = secondNumberText.toString().toInt()
+
+        val result = when(operatorText.toString()) {
+            "+" -> firstNumber + secondNumber
+            "-" -> firstNumber - secondNumber
+            else -> "잘못된 수식입니다."
+        }.toString()
+        binding.resultTextView.text = result
+    }
+
+    fun operatorClicked(view: View) {
+        val operatorString = (view as? Button)?.text.toString() ?: ""
+
+        if (firstNumberText.isEmpty()) {
+            Toast.makeText(this, "먼저 숫자를 입력해 주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if(secondNumberText.isNotEmpty()) {
+            Toast.makeText(this, "한개의 연산자에 대해서만 연산이 가능합니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // data를 업데이트
+        operatorText.append(operatorString)
+        updateEquationTextView()
+    }
+
+    private fun updateEquationTextView() {
+        binding.equationTextView.text = "$firstNumberText $operatorText $secondNumberText"
+
+    }
+}
